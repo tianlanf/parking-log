505f56a3a9a7a106a57a07b71f385941c5953504
Yafei Chen <yafchen@thoughtworks.com>
2020-05-27 10:54:55 +0800

    task 3/4: cannot get car without ticket or with fake ticket

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class ParkingLotTest {
 
@@ -23,4 +24,21 @@ public class ParkingLotTest {
         assertNotNull(result);
         assertEquals(myCar, result);
     }
+
+    @Test
+    public void shouldNotGetMyCarWithoutMyTicket() {
+        Car myCar = new Car("123");
+
+        parkingLot.park(myCar);
+        Car result = parkingLot.pickUp(null);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldNotGetCarWithFakeTicket() {
+        Car result = parkingLot.pickUp(new CarTicket());
+        assertNull(result);
+    }
+
 }
